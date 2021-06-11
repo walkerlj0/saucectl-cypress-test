4272ce6506afb3ab2b101c19bcf1a9f232437d35
Vera <vera.wei@saucelabs.com>
2021-06-11 12:41:38 -0700

    delete unsed files

--- coderoad/test/TestWritting.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-describe('Tutorial Saucectl Cypress Test', function() {
-  describe('First Test Writting', function() {
-    it('', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
